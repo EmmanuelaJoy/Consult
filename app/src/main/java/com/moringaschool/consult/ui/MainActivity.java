5e988c5acf9f3fa2beda45f74f99bd4850b55826
Emmanuela Joy <emmajoy81@gmail.com>
2021-06-15 12:04:08 +0300

    create sub-packages

--- app/src/main/java/com/moringaschool/consult/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/consult/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.moringaschool.consult;
+package com.moringaschool.consult.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.moringaschool.consult.R;
 
 public class MainActivity extends AppCompatActivity {
 
