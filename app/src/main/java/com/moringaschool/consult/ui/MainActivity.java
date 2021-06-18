18e697c2485dd3e28baee71cdeec6486da7b5080
Mugure-Inez <enezmugure@gmail.com>
2021-06-18 09:13:31 +0300

    changes to chat page

--- app/src/main/java/com/moringaschool/consult/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/consult/ui/MainActivity.java
@@ -1,16 +1,180 @@
 package com.moringaschool.consult.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.moringaschool.consult.Fragments.APIService;
 import com.moringaschool.consult.R;
+import com.moringaschool.consult.Fragments.ChatFragment;
+import com.moringaschool.consult.Fragments.ProfileFragment;
+import com.moringaschool.consult.Fragments.UsersFragment;
+import com.moringaschool.consult.Models.Chat;
+import com.moringaschool.consult.Models.User;
+import com.moringaschool.consult.Notifications.Client;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
 
 public class MainActivity extends AppCompatActivity {
+    CircleImageView circleImageView;
+    TextView userName;
+    FirebaseUser firebaseUser;
+    DatabaseReference databaseReference;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+        // to fetch data from data base
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // fetch data
+                /*
+                The class attributes must be rhe same names as in database as they are used as keys to retruve the values
+                **/
+                User user = snapshot.getValue(User.class);
+
+                // display data
+                assert user != null;
+                userName.setText(user.getUsername());
+
+                //set new image
+                if (user.getImageURL().equals("default"))
+                    circleImageView.setImageResource(R.mipmap.ic_launcher_round);
+                else
+                    Glide.with(getApplicationContext()).load(user.getImageURL()).into(circleImageView);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    void initialize() {
+        circleImageView = findViewById(R.id.profile_picture);
+        userName = findViewById(R.id.username_main);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        // initialize tool bar
+        toolbar = findViewById(R.id.tool_bar_Main);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar(), "No tool bar found").setTitle("");
+
+        //initialize tab layout
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.tab_view_id);
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Chats");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int unRead = 0;
+
+                // This second parameter is used to fill only the constructor of view page adapter
+                // as the old constructor is deprecated
+                viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Chat chat = dataSnapshot.getValue(Chat.class);
+                    assert chat != null;
+                    if (chat.getReceiver().equals(firebaseUser.getUid()) && !chat.isSeen()) {
+                        unRead += 1;
+                    }
+                }
+                if (unRead == 0) {
+                    viewPagerAdapter.addFragment(new ChatFragment(), "Chat");
+                } else {
+                    viewPagerAdapter.addFragment(new ChatFragment(), "(" + unRead + ") " + "Chat");
+                }
+                viewPagerAdapter.addFragment(new UsersFragment(), "Users");
+                viewPagerAdapter.addFragment(new ProfileFragment(), "Profile");
+                viewPager.setAdapter(viewPagerAdapter);
+                tabLayout.setupWithViewPager(viewPager);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //get data from data base using uid which is in fire base  user
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+    }
+
+    void status(String status) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("status", status);
+
+        databaseReference.updateChildren(hashMap);
+    }
+
+    // this two methods are overridden related to activity life cycle
+    // here is link for more understanding :
+    // https://developer.android.com/guide/components/activities/activity-lifecycle#:~:text=Use%20the%20onPause()%20method,you%20expect%20to%20resume%20shortly.
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        status("online");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        status("offline");
+    }
+
+    //This two functions are used create sub menu in top right of screen
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(MainActivity.this, StartActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            return true;
+        }
+        return false;
     }
 }
