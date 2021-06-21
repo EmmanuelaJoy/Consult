977a8d4dc369a13926eacc5f5146ff066351ba7b
Emmanuela Joy <emmajoy81@gmail.com>
2021-06-21 08:54:26 +0300

    modify color codes

--- app/src/main/java/com/moringaschool/consult/ui/Dashboard.java
+++ app/src/main/java/com/moringaschool/consult/ui/Dashboard.java
@@ -1,15 +1,24 @@
 package com.moringaschool.consult.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.consult.R;
 
 public class Dashboard extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    MenuItem home;
+    MenuItem tasks;
+    MenuItem page;
+    MenuItem profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +27,8 @@ public class Dashboard extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setBackground(null);
+
+
     }
+
 }
