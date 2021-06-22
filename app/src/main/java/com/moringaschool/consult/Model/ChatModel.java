a7eb818e48b6347a8028a41918c7eb7448eaa121
Mugure-Inez <enezmugure@gmail.com>
2021-06-22 19:54:28 +0300

    chat page updated and working

---
+++ app/src/main/java/com/moringaschool/consult/Model/ChatModel.java
@@ -0,0 +1,30 @@
+package com.moringaschool.consult.Model;
+
+public class ChatModel {
+    String user, message, timeStamp;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
+
