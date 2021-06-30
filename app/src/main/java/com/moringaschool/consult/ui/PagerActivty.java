5bfdb415e9ba56daad04742e519c10e7cf5f117d
Emmanuela Joy <emmajoy81@gmail.com>
2021-06-30 20:30:51 +0300

    add a title and icon

--- app/src/main/java/com/moringaschool/consult/ui/PagerActivty.java
+++ app/src/main/java/com/moringaschool/consult/ui/PagerActivty.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.moringaschool.consult.R;
+
 public class PagerActivty extends AppCompatActivity {
 
     @Override
