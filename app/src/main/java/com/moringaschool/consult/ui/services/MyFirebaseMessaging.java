3ad5055cf932bbdb69d9076f92ed06820e993daa
Mugure-Inez <enezmugure@gmail.com>
2021-06-30 06:58:56 +0300

    ft/alert

--- app/src/main/java/com/moringaschool/consult/ui/services/MyFirebaseMessaging.java
+++ app/src/main/java/com/moringaschool/consult/ui/services/MyFirebaseMessaging.java
@@ -2,6 +2,7 @@ package com.moringaschool.consult.ui.services;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 import android.util.Log;
