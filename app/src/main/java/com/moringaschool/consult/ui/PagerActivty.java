b2b6e3eb592b464aa8d4417a302badadd9ba5ad2
Emmanuela Joy <emmajoy81@gmail.com>
2021-06-30 20:04:27 +0300

    create a pager activity

---
+++ app/src/main/java/com/moringaschool/consult/ui/PagerActivty.java
@@ -0,0 +1,14 @@
+package com.moringaschool.consult.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PagerActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pager);
+    }
+}
