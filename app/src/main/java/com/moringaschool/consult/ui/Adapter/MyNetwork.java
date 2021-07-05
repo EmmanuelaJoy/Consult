e22d496ffa04048439222d4b7f00620e1395b38e
Emmanuela Joy <emmajoy81@gmail.com>
2021-07-05 10:51:55 +0300

    modify dashboard layout

---
+++ app/src/main/java/com/moringaschool/consult/ui/Adapter/MyNetwork.java
@@ -0,0 +1,142 @@
+package com.moringaschool.consult.ui.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.auth.data.model.User;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.Query;
+import com.moringaschool.consult.R;
+import com.moringaschool.consult.ui.Dashboard;
+import com.moringaschool.consult.ui.Model.Chatslist;
+import com.moringaschool.consult.ui.Model.Users;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.net.URL;
+import java.util.List;
+
+public class MyNetwork extends RecyclerView.Adapter<MyNetwork.NetworkViewHolder> {
+
+    List<Users> usersList;
+
+    public MyNetwork(List<Users> users) {
+        this.usersList = users;
+    }
+
+    @Override
+    public NetworkViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_network, parent, false);
+        return new NetworkViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyNetwork.NetworkViewHolder holder, int position) {
+        holder.mName.setText(usersList.get(position).getUsername());
+//        holder.mProfession.setText(usersList.get(position).getStatus());
+        if(usersList.get(position).getImageURL() != "") {
+            Picasso.get().load(usersList.get(position).getImageURL()).into(holder.mImage);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (usersList != null) {
+            return this.usersList.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public class NetworkViewHolder extends RecyclerView.ViewHolder {
+        TextView mName;
+        //        TextView mProfession;
+        ImageView mImage;
+
+        public NetworkViewHolder(View view) {
+            super(view);
+            mName = view.findViewById(R.id.userName);
+//            mProfession = view.findViewById(R.id.userProfession);
+            mImage = view.findViewById(R.id.userImage);
+
+        }
+
+    }
+}
+
+//    TextView mName;
+//    TextView mProfession;
+//    ImageView mImage;
+//
+//    public MyNetwork(@NonNull View itemView) {
+//        super(itemView);
+//    }
+//
+//    public void setItem(Dashboard dashboard,  String username, String imageURL){
+//        mImage = itemView.findViewById(R.id.userImage);
+//        mName = itemView.findViewById(R.id.userName);
+//        mProfession = itemView.findViewById(R.id.userProfession);
+//
+//        Picasso.get().load(imageURL).into(mImage);
+//        mName.setText(username);
+//    }
+
+
+//public class MyNetwork extends FirebaseRecyclerAdapter<MyNetwork.NetworkViewHolder> {
+//
+//    List<Chatslist> networkList;
+//    private Context mContext;
+//    List<Users> mUsers;
+//    UserAdapter mAdapter;
+//    FirebaseUser firebaseUser;
+//
+//    @Override
+//    public MyNetwork.NetworkViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_network, parent, false);
+//        return new NetworkViewHolder(view);
+//    }
+//
+//    @Override
+//    protected void populateViewHolder(MyNetwork.NetworkViewHolder viewHolder, Object o, int i) {
+//
+//        viewHolder.mName.setText(mUsers.get(i).getUsername());
+//        viewHolder.mProfession.setText(mUsers.get(i).getStatus());
+//        if(mUsers.get(i).getImageURL() != ""){
+//            Picasso.get().load(mUsers.get(i).getImageURL()).into(viewHolder.mImage));
+//        }
+//    }
+
+//    public class NetworkViewHolder extends RecyclerView.ViewHolder {
+//
+//        TextView mName;
+//        TextView mProfession;
+//        ImageView mImage;
+//
+//
+//        public NetworkViewHolder(View view) {
+//            super(view);
+//            mName = view.findViewById(R.id.userName);
+//            mProfession = view.findViewById(R.id.userProfession);
+//            mImage = view.findViewById(R.id.userImage);
+//
+//        }
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//
+//
+//}
--- app/src/main/java/com/moringaschool/consult/ui/Dashboard.java
+++ app/src/main/java/com/moringaschool/consult/ui/Dashboard.java
@@ -1,46 +1,139 @@
 package com.moringaschool.consult.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.data.model.User;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.moringaschool.consult.R;
+import com.moringaschool.consult.ui.Adapter.MyNetwork;
+import com.moringaschool.consult.ui.Model.Chatslist;
+import com.moringaschool.consult.ui.Model.Users;
+
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
 
 public class Dashboard extends AppCompatActivity {
 
+    List<Users> mUsersList;
+
     BottomNavigationView bottomNavigationView;
-    String user_email, user_username, user_password, user_fullnames;
-    String names, email, password, username;
     TextView title;
+    ImageView imageView;
+    RecyclerView recyclerView;
+    DatabaseReference reference;
+    DatabaseReference reference1;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    MyNetwork myNetworkAdapter;
+    LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.dashboard);
 
         title = findViewById(R.id.title);
+        imageView = findViewById(R.id.profile);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setBackground(null);
 
-//        //user-details
-//        Intent intent = getIntent();
-//        user_email = intent.getStringExtra("email");
-//        user_username = intent.getStringExtra("username");
-//        user_password = intent.getStringExtra("password");
-//        user_fullnames = intent.getStringExtra("fullnames");
+        recyclerView = findViewById(R.id.myNetworkRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+//        reference1 = firebaseDatabase.getReference("Users");
+        mUsersList = new ArrayList<>();
+//        reference1.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+//                Users users = snapshot.getValue(Users.class);
+//                mUsersList.add(users);
+//            }
 //
-//        title.setText("Welcome " + user_fullnames);
-//        names = user_fullnames;
-//        password = user_password;
-//        email = user_email;
-//        username = user_username;
+//            @Override
+//            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+//
+//            }
+//        });
+        myNetworkAdapter = new MyNetwork(mUsersList);
+        recyclerView.setAdapter(myNetworkAdapter);
+
+
+//        myNetworkAdapter = new MyNetwork(Users.class, R.layout.my_network, MyNetwork.NetworkViewHolder.class, reference1);
+//        recyclerView.setAdapter(myNetworkAdapter);
+
+
+
+//        layoutManager = new LinearLayoutManager(this);
+//        recipesRecyclerView.setLayoutManager(layoutManager);
+//        mRecipeListAdapter = new RecipeListAdapter(this, allRecipesClassList);
+//        recipesRecyclerView.setAdapter(mRecipeListAdapter);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+        reference.keepSynced(true);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Users users = snapshot.getValue(Users.class);
+                mUsersList.add(users);
+
+                title.setText("Welcome\n" + users.getUsername());
+
+                if (users.getImageURL().equals("default")) {
+
+                    imageView.setImageResource(R.drawable.ic_account);
+                } else {
+                    Glide.with(getApplicationContext()).load(users.getImageURL()).into(imageView);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                options();
+            }
+        });
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -53,15 +146,11 @@ public class Dashboard extends AppCompatActivity {
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.profileM:
-//                        profileIntent.putExtra("email", email);
-//                        profileIntent.putExtra("username", username);
-//                        profileIntent.putExtra("fullnames", names);
-//                        profileIntent.putExtra("password", password);
                         startActivity(new Intent(getApplicationContext(), UserProfile.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.chatButton:h:
-                        startActivity(new Intent(getApplicationContext(), TextActivity.class));
+                    startActivity(new Intent(getApplicationContext(), TextActivity.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.pageM:
@@ -75,6 +164,44 @@ public class Dashboard extends AppCompatActivity {
 
     }
 
+    public void options(){
+        String[] options = {"View Profile", "Log Out"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(Dashboard.this);
+        builder.setTitle("Choose an Option");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+
+
+                if (i==0) {
+
+                    profile();
+
+                }
+
+                if (i==1) {
+
+                    logout();
+                }
+            }
+        });
+
+        builder.create().show();
+    }
+
+    public void logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(Dashboard.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void profile(){
+        startActivity(new Intent(getApplicationContext(), UserProfile.class));
+        overridePendingTransition(0,0);
+    }
+
+
     public void message(View view){
         startActivity(new Intent(getApplicationContext(), TextActivity.class));
         overridePendingTransition(0,0);
@@ -86,6 +213,18 @@ public class Dashboard extends AppCompatActivity {
             super.onBackPressed();
             finish();
         }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
 
     }
 }
