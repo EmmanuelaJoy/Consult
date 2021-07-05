c37d55365a7431f39fe6a8fed083e1158915892d
Emmanuela Joy <emmajoy81@gmail.com>
2021-07-05 11:49:59 +0300

    add a profession, password and phone number to user details

--- app/src/main/java/com/moringaschool/consult/ui/Model/Users.java
+++ app/src/main/java/com/moringaschool/consult/ui/Model/Users.java
@@ -3,18 +3,53 @@ package com.moringaschool.consult.ui.Model;
 public class Users {
 
 
-    String username, imageURL, id, status;
+    String username, imageURL, id, status, email, profession, phone, password;
 
-    public Users(String username, String imageURL, String id, String status) {
+    public Users(String username, String imageURL, String id, String status, String email, String profession, String phone, String password) {
         this.username = username;
         this.imageURL = imageURL;
         this.id = id;
         this.status = status;
+        this.email = email;
+        this.profession = profession;
+        this.phone = phone;
+        this.password = password;
     }
 
     public Users() {
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
 
     public String getUsername() {
         return username;
--- app/src/main/java/com/moringaschool/consult/ui/UserProfile.java
+++ app/src/main/java/com/moringaschool/consult/ui/UserProfile.java
@@ -3,12 +3,14 @@ package com.moringaschool.consult.ui;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,53 +26,51 @@ import org.jetbrains.annotations.NotNull;
 
 public class UserProfile extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
     DatabaseReference reference;
-    FirebaseUser firebaseUser;
-    TextView name,email;
+    TextView name,profession;
     ImageView profile;
-    String user_email, user_fullnames, user_id;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-//        profile.findViewById(R.id.profileImage);
-//        name = findViewById(R.id.displayName);
-//        email = findViewById(R.id.displayUsername);
-//
-//        mAuth = FirebaseAuth.getInstance();
+        name = findViewById(R.id.displayName);
+        profession = findViewById(R.id.displayProfession);
+        profile = findViewById(R.id.profileImage);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+        reference.keepSynced(true);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Users users = snapshot.getValue(Users.class);
+
+                name.setText(users.getUsername());
+                profession.setText(users.getPassword());
+
+                if (users.getImageURL().equals("default")) {
+
+                    profile.setImageResource(R.drawable.ic_account);
+                } else {
+                    Glide.with(getApplicationContext()).load(users.getImageURL()).into(profile);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
-//        private void showAllUserData() {
-//            Intent profileIntent = getIntent();
-//            user_email = profileIntent.getStringExtra("email");
-//            user_fullnames = profileIntent.getStringExtra("fullnames");
-//            user_id = firebaseUser.getUid();
-//
-//            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
-//            Query checkUser = reference.orderByChild(user_id);
-//            checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
-//                @Override
-//                public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-//                    if(snapshot.exists()){
-//                        String emailFromDB= snapshot.child(user_id).child("status").getValue(String.class);
-//                        String fullNamesFromDB= snapshot.child(user_id).child("username").getValue(String.class);
-//                        email.setText(emailFromDB);
-//                        name.setText(fullNamesFromDB);
-//                    }
-//                }
-//
-//                @Override
-//                public void onCancelled(@NonNull @NotNull DatabaseError error) {
-//
-//                }
-//            });
-//        }
 
     public void user(View view){
         Intent intent = new Intent(UserProfile.this, UserDetails.class);
